17bdab8f38b28ea2ddaf301fc3e13e24761e0e71
Gm Dev <gmmoose17@gmail.com>
2024-08-03 18:41:53 +0800

    added a feature:  drinking animation

--- src/components/Drink.jsx
+++ src/components/Drink.jsx
@@ -1,40 +1,46 @@
 import { useState, useEffect } from "react";
 import { CiBeerMugFull } from "react-icons/ci";
 import { SlRefresh } from "react-icons/sl";
+import Lottie from "lottie-react";
+import animationData from "../assets/drink.json";
 
 const Drink = ({ setTheDrink }) => {
-  const [animate, setAnimate] = useState(false);
-  const [effect, setEffect] = useState(false);
+	const [animate, setAnimate] = useState(false);
+	const [effect, setEffect] = useState(false);
 
-  useEffect(() => {
-    setAnimate(true);
-  }, []);
-  return (
-    <>
-      <div className="grid place-content-center p-5">
-        <SlRefresh
-          className={`${effect ? "" : "animate-sparkle"}  ${effect && "animate-restarting"} text-4xl text-white`}
-          onClick={() => {
-            setEffect(true);
-            setTimeout(() => {
-              setTheDrink(false);
-            }, 1000);
-          }}
-          onAnimationEnd={() => setEffect(false)}
-        />
-      </div>
-      <div className="h-85vh w-full grid place-content-center ">
-        {/* Drink content goes here */}
-        <CiBeerMugFull
-          className="text-5xl text-yellow-600 transition-all duration-1000"
-          style={{
-            transform: `scale(${animate ? 5 : 1})`,
-          }}
-          onAnimationEnd={() => setEffect(false)}
-        />
-      </div>
-    </>
-  );
+	useEffect(() => {
+		setAnimate(true);
+		// setTriggerReward(true);
+	}, []);
+	return (
+		<>
+			<div className="grid place-content-center p-5">
+				<SlRefresh
+					className={`${effect ? "" : "animate-sparkle"}  ${
+						effect && "animate-restarting"
+					} text-4xl text-white`}
+					onClick={() => {
+						setEffect(true);
+						setTimeout(() => {
+							setTheDrink(false);
+						}, 1000);
+					}}
+					onAnimationEnd={() => setEffect(false)}
+				/>
+			</div>
+			<div className="h-85vh w-full grid place-content-center ">
+				{/* Drink content goes here */}
+				{/* <CiBeerMugFull
+					className="text-5xl text-yellow-600 transition-all duration-1000"
+					style={{
+						transform: `scale(${animate ? 5 : 1})`,
+					}}
+					onAnimationEnd={() => setEffect(false)}
+				/> */}
+				<Lottie animationData={animationData} loop={true} />
+			</div>
+		</>
+	);
 };
 
 export default Drink;
